e1cc902e0ba3a9b6510d89ed60d50fee61d41477
simonecorsini <s.corsini@liquidlabs.de>
2015-11-03 14:39:17 +0100

    Added a tooltip feature for the column charts

--- examples/src/components/Menu.js
+++ examples/src/components/Menu.js
@@ -1,19 +1,20 @@
-var React = require('react'); 
+var React = require('react');
 var Menu = React.createClass({
 	render: function() {
 		return (
 			<div>
 			<nav className="menu">
-				<h1 className="link"> <a href="/"> Usage </a> </h1> 
-				<h1 className="link"> <a href="/examples/home"> Examples </a> </h1> 
-					<h2 className="link"> <a href="/examples/BarCharts"> Bar Charts </a> </h2> 
-					<h2 className="link" ><a href="/examples/BubbleCharts">    Bubble Charts </a> </h2> 
-					<h2 className="link" ><a href="/examples/Histograms ">    Histograms </a> </h2> 
-				 	<h2 className="link" ><a href="/examples/LineCharts">   Line Charts </a> </h2> 
+				<h1 className="link"> <a href="/"> Usage </a> </h1>
+				<h1 className="link"> <a href="/examples/home"> Examples </a> </h1>
+					<h2 className="link"> <a href="/examples/BarCharts"> Bar Charts </a> </h2>
+					<h2 className="link" ><a href="/examples/BubbleCharts">    Bubble Charts </a> </h2>
+					<h2 className="link" ><a href="/examples/Histograms ">    Histograms </a> </h2>
+				 	<h2 className="link" ><a href="/examples/LineCharts">   Line Charts </a> </h2>
+				  <h2 className="link" ><a href="/examples/ColumnCharts">   Column Charts </a> </h2>
 				 	<h2 className="link" ><a href="/examples/ScatterCharts">   Scatter Charts </a> </h2>
 				 	<h2 className="link" ><a href="/examples/ManyMore">   Many more </a> </h2>
-				<h1 className="link"> <a href="https://github.com/RakanNimer/react-google-charts/"> Source </a> </h1> 
-				<h1 className="link"> <a href="https://github.com/RakanNimer/react-google-charts/tree/gh-pages"> Examples Source </a> </h1> 
+				<h1 className="link"> <a href="https://github.com/RakanNimer/react-google-charts/"> Source </a> </h1>
+				<h1 className="link"> <a href="https://github.com/RakanNimer/react-google-charts/tree/gh-pages"> Examples Source </a> </h1>
 			</nav>
 			</div>
 		);
