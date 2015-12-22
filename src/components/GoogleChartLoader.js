660cbca8f784f06862a767183e4550797d3995e7
Pierre Kraft <pierre@youpic.com>
2015-12-22 16:12:44 +0100

    Replace jquery with scriptjs and update to react 0.14

--- src/components/GoogleChartLoader.js
+++ src/components/GoogleChartLoader.js
@@ -2,42 +2,32 @@
 
 // Based on http://blog.arkency.com/2014/09/react-dot-js-and-google-charts/
 var q = require('q');
-var $ = require('jquery');
+var script = require("scriptjs")
 
 var GoogleChartLoader = function(){
 
 	this.is_loading = false;
 	this.is_loaded = false;
 	this.google_promise = q.defer();
-	this.url = "https://www.google.com/jsapi";
-	this.packages = ["corechart"];
-	this.version = "1";
-	this.init = function(packages, version) {
 
+	var self = this;
+
+	this.init = function(packages, version) {
+		// Charts can only be loaded once because of the way google implemented this
+		// Remember to load all packages you need at the first call
 		if (this.is_loading) {
 			return this.google_promise.promise;
 		}
+		script("https://www.gstatic.com/charts/loader.js", function() {
+			google.charts.load(version || 'current', {packages: packages || ['corechart']});
+			google.charts.setOnLoadCallback(function() {
+			self.is_loaded = true;
+  			self.google_promise.resolve();
+			})
+		})
 
-		this.is_loading = true;
-		var self = this;
-
-	 	var options = {
-	    	dataType: "script",
-	    	cache: true,
-	    	url: this.url,
-	  	};
-	  	$.ajax(options).done(
-	  		function(){
-	    		google.load("visualization", version || self.version, {
-	      			packages: packages || self.packages,
-	      			callback: function() {
-	      				self.is_loaded = true;
-	        			self.google_promise.resolve();
-	      			}
-	    		});
-	    });
-	    return this.google_promise.promise;
-	}
+		return this.google_promise.promise;
+	};
 };
 
 module.exports = new GoogleChartLoader();
