352f3d650937eecc0cf42db5a74586f008f6df42
RakanNimer <rakannimer@gmail.com>
2016-08-31 18:15:08 +0300

    Removed build files

--- es6/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import React from 'react';
-
-//import googleChartLoader from './components/GoogleChartLoader';
-import Chart from './components/Chart';
-//import SAMPLE_DATA from './constants/SAMPLE_DATA';
-
-export default { Chart: Chart };
