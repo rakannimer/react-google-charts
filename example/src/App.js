51166e2b57dc05b43a95db7d63cb000bb87ab966
RakanNimer <rakannimer@gmail.com>
2016-09-24 11:20:53 +0300

    Added simple example app.

---
+++ example/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Charts from './Charts';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Charts />
+      </div>
+    );
+  }
+}
+
+export default App;
