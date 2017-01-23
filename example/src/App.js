1be4e685b7156f07bc325ef1f44c506aede10126
RakanNimer <rakannimer@gmail.com>
2017-01-23 19:20:23 +0200

    Added example with cra

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import Charts from './Charts';
+import React from 'react';
+import { render } from 'react-dom';
+import { Chart } from 'react-google-charts';
 
-class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Charts />
+      <div className={'my-pretty-chart-container'}>
+        <Chart
+          chartType="ScatterChart"
+          data={[['Age', 'Weight'], [8, 12], [4, 5.5], [1, 2]]}
+          options={{}}
+          graph_id="ScatterChart"
+          width="100%"
+          height="400px"
+          legend_toggle
+        />
       </div>
     );
   }
 }
-
-export default App;
