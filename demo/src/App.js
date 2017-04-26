cdc33f85442f4fb94c54c62e8f7dcc3da9594db3
Alessander Franca <alessander70@gmail.com>
2017-04-26 19:57:45 -0300

    div id fixed at index.js, imports fixed and state to functional component

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Chart } from 'react-google-charts';
+import Chart from '../../src/components/Chart';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className={'my-pretty-chart-container'}>
-        <Chart
-          chartType="ScatterChart"
-          data={[['Age', 'Weight'], [8, 12], [4, 5.5], [1, 2]]}
-          options={{}}
-          graph_id="ScatterChart"
-          width="100%"
-          height="400px"
-          legend_toggle
-        />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className={'my-pretty-chart-container'}>
+    <Chart
+      chartType="ScatterChart"
+      data={[['Age', 'Weight'], [8, 12], [4, 5.5], [1, 2]]}
+      options={{}}
+      graph_id="ScatterChart"
+      width="100%"
+      height="400px"
+      legend_toggle
+    />
+  </div>
+);
+
+export default App;
--- demo/src/index.js
+++ demo/src/index.js
@@ -5,5 +5,5 @@ import './index.css';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('demo')
 );
