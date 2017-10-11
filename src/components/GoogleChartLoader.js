448ac20cff998839802331f46df6686f821609b7
Mike Bridge <mike@bridgecanada.com>
2017-10-11 11:09:17 -0600

    add mapsApiKey for GeoChart

--- src/components/GoogleChartLoader.js
+++ src/components/GoogleChartLoader.js
@@ -11,15 +11,19 @@ const googleChartLoader = {
   isLoaded: false,
   isLoading: false,
   initPromise: {},
-  init: function init(packages, version) {
-    debug('init', packages, version);
+  init: function init(packages, version, mapsApiKey) {
+    debug('init', packages, version, mapsApiKey);
     if (this.isLoading || this.isLoaded) {
       return this.initPromise;
     }
     this.isLoading = true;
     this.initPromise = new Promise((resolve) => {
       script('https://www.gstatic.com/charts/loader.js', { success: () => {
-        window.google.charts.load(version || 'current', { packages: packages || ['corechart'] });
+        const opts = { packages: packages || ['corechart'] };
+        if (mapsApiKey) {
+          opts.mapsApiKey = mapsApiKey;
+        }
+        window.google.charts.load(version || 'current', opts);
         window.google.charts.setOnLoadCallback(() => {
           debug('Chart Loaded');
           this.isLoaded = true;
