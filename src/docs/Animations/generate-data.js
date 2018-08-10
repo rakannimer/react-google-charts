41db59b392f9a677fbdd7c071523732cd83c9207
rakannimer <rakannimer@gmail.com>
2018-08-10 19:18:04 +0300

    [Docs] Animation example

---
+++ src/docs/Animations/generate-data.js
@@ -0,0 +1,15 @@
+const rand = n => {
+  return n;
+  // return Math.random() * n;
+};
+export const generateData = () => {
+  return [
+    ["Age", "Weight"],
+    [rand(8), rand(12)],
+    [rand(4), rand(5.5)],
+    [rand(1), rand(14)],
+    [rand(4), rand(5)],
+    [rand(3), rand(3.5)],
+    [rand(6), rand(7)]
+  ];
+};
