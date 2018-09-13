87017a166ec5c503cd8a7ce1bf68b9a297d85ea9
rakannimer <rakannimer@gmail.com>
2018-09-13 14:13:35 +0300

    docs: Update geo-chart sandbox to show regionClick functionality. #244

--- sandboxes/geo-chart/index.js
+++ sandboxes/geo-chart/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Chart from "react-google-charts";
+import * as React from "react";
+import { render } from "react-dom";
+import { Chart } from "../src";
 
 const data = [
   ["Country", "Popularity"],
@@ -15,11 +15,29 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Chart chartType="GeoChart" width="100%" height="400px" data={data} />
+        <Chart
+          chartEvents={[
+            {
+              eventName: "select",
+              callback:({chartWrapper}) => {
+                const chart = chartWrapper.getChart();
+                const [selection] = chart.getSelection();
+                const region = data[selection.row]
+                console.log("Selected : "+region)
+            }
+          ]}
+          chartType="GeoChart"
+          width="100%"
+          height="400px"
+          data={data}
+          options={{
+            enableRegionInteractivity: true,
+            dataMode: "regions"
+          }}
+        />
       </div>
     );
   }
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+render(<App />, document.getElementById("app"));
