7473783d9e7c37c97da2368160612725e50457d9
norama <norama.matema@gmail.com>
2019-03-06 14:10:18 +0100

    ie11 compatibility: onLoad waits for full loading of google charts (#286)
    Solution for IE11 compatibility issue: https://github.com/rakannimer/react-google-charts/issues/270
    
    Remark: pre-commit hook was a bit problematic, I had to run `yarn add  @types/node@8.0.0` locally,
    otherwise tests could not run due to some duplicate symbol error.

--- src/ReactGoogleCharts.tsx
+++ src/ReactGoogleCharts.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import {
+  GoogleViz,
   ReactGoogleChartProps,
   ReactGoogleChartState,
   ReactGoogleChartPropsWithDefaults
@@ -14,11 +15,16 @@ export class Chart extends React.Component<
   ReactGoogleChartProps,
   ReactGoogleChartState
 > {
+
+  _isMounted = false;
+
   state = {
     loadingStatus: "loading" as ReactGoogleChartState["loadingStatus"],
     google: null as ReactGoogleChartState["google"]
   };
+
   static defaultProps = chartDefaultProps;
+
   render() {
     const {
       chartLanguage,
@@ -42,19 +48,68 @@ export class Chart extends React.Component<
         )}
         <GoogleChartLoader
           {...{ chartLanguage, chartPackages, chartVersion, mapsApiKey }}
-          onLoad={google => {
-            this.setState({
-              loadingStatus: "ready",
-              google
-            });
-          }}
-          onError={() => {
-            this.setState({
-              loadingStatus: "errored"
-            });
-          }}
+          onLoad={this.onLoad}
+          onError={this.onError}
         />
       </ContextProvider>
+    );
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  onLoad = (google: GoogleViz) => {
+    if (this.isFullyLoaded(google)) {
+      this.onSuccess(google);
+    } else {
+      // IE11: window.google is not fully set, we have to wait
+      const id = setInterval(() => {
+        const google = (window as Window & {
+          google?: GoogleViz;
+        }).google;
+
+        if (this._isMounted) {
+          if (google && this.isFullyLoaded(google)) {
+            clearInterval(id);
+            this.onSuccess(google);
+          }
+        } else {
+          clearInterval(id);
+        }
+
+      }, 1000);
+    }
+  };
+
+  onSuccess = (google: GoogleViz) => {
+    this.setState({
+      loadingStatus: "ready",
+      google
+    });
+  };
+
+  onError = () => {
+    this.setState({
+      loadingStatus: "errored"
+    });
+  };
+
+  isFullyLoaded(google: GoogleViz) {
+    const { controls, toolbarItems, getChartEditor } = this.props;
+
+    return (
+      google &&
+      google.visualization &&
+      google.visualization.ChartWrapper &&
+      google.visualization.Dashboard &&
+      (!controls || google.visualization.ChartWrapper) &&
+      (!getChartEditor || google.visualization.ChartEditor) &&
+      (!toolbarItems || google.visualization.drawToolbar)
     );
   }
 }
