1dd7efb08f716a388fff0cea8e9be756e155f0dd
Dan Onoshko <danon0404@gmail.com>
2021-11-22 16:55:46 +0700

    docs(sandboxes): upgrade sandboxes (#440)
    rewritten to TypeScript and functional components

---
+++ sandboxes/donut-chart/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7], // CSS-style declaration
+];
+
+export const options = {
+  title: "My Daily Activities",
+  pieHole: 0.4,
+  is3D: false,
+};
+
+export function App() {
+  return (
+    <Chart
+      chartType="PieChart"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+    />
+  );
+}
